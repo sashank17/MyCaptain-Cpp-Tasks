a8c10549ab447bd91a419fc3ad00365ad85d58f2
sashank17 <44266341+sashank17@users.noreply.github.com>
2020-06-08 12:37:03 +0530

    Completed Task2 - Product

---
+++ Task2 - Product.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double x;
+    double y;
+    cout << "Enter two numbers: " << endl;
+    cin >> x;
+    cin >> y;
+    cout << "Product = " << (x * y) << endl;
+}
