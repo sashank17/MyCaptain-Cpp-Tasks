ae611fa79a91c1a4ec42c5a8f3f9487171127e9b
sashank17 <44266341+sashank17@users.noreply.github.com>
2020-06-08 12:36:06 +0530

    Completed Task1 - Size of data types

---
+++ Task1 - Size of data types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    int b;
+    float c;
+    double d;
+    cout << "Size of char: " << sizeof(a) << " byte" << endl;
+    cout << "Size of int: " << sizeof(b) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(c) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(d) << " bytes" << endl;
+}
